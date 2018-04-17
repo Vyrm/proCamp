f1be7104f6d86c8057b28fc66c2f54230aa74dee
Serhii Onyshchenko <serhii.onyshchenko@synapse.com>
2018-04-17 18:16:23 +0300

    annotation servlet config

--- src/main/java/com/garden/servlet/BouquetServlet.java
+++ src/main/java/com/garden/servlet/BouquetServlet.java
@@ -1,14 +1,13 @@
 package com.garden.servlet;
 
-import org.springframework.stereotype.Component;
-
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Component
+@WebServlet(name = "bouquetServlet", urlPatterns = "/")
 public class BouquetServlet extends HttpServlet {
 
     @Override
--- src/main/java/com/garden/servlet/ResultServlet.java
+++ src/main/java/com/garden/servlet/ResultServlet.java
@@ -6,12 +6,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
+@WebServlet(name = "resultServlet", urlPatterns = "/getId")
 public class ResultServlet extends HttpServlet {
 
     private BouquetService bouquetService;
